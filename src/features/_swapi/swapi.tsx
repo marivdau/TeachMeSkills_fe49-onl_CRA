6a71b4d048d4793a199541a9e1d370d1afd4ecb9
marivdau <87074474+marivdau@users.noreply.github.com>
2023-10-26 21:07:53 +0300

    webpack lesson

--- src/features/_swapi/swapi.tsx
+++ src/features/_swapi/swapi.tsx
@@ -3,18 +3,18 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import { getSwapi } from './swapi.slice';
 
 export const Swapi: React.FC = () => {
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
-  const { swapi } = useAppSelector(({ swapi }) => swapi);
-  useEffect(() => {
-    dispatch(getSwapi());
-  }, [dispatch]);
+  // const { swapi } = useAppSelector(({ swapi }) => swapi);
+  // useEffect(() => {
+  //   dispatch(getSwapi());
+  // }, [dispatch]);
   return (
     <div>
-      <div>{swapi.name}</div>
+      {/* <div>{swapi.name}</div>
       <div>{swapi.rotation_period}</div>
       <div>{swapi.gravity}</div>
-      <a href={swapi.url}>Link</a>
+      <a href={swapi.url}>Link</a> */}
     </div>
   );
 };
